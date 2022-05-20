77cf0adb0a573d0dd06c654e89db3cf6a8f81825
Zsolt Kebel <kebel.zsolt@gmail.com>
2022-05-20 10:53:47 +0200

    Initial Commit

---
+++ Shared/Quick_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_ShareApp.swift
+//  Shared
+//
+//  Created by Zsolt Kébel on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Quick_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
