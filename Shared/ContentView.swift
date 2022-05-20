58a72457565812e25d8a8ffdf159e1c79366efec
Zsolt Kebel <kebel.zsolt@gmail.com>
2022-05-20 10:59:48 +0200

    Set up core functionality with QR code generated from last pasteboard text item.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let address = "https://api.qrserver.com/v1/create-qr-code/?data="
+    let test = "https://api.qrserver.com/v1/create-qr-code/?data=https://www.google.com"
+    
+    @State var url: String?
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            if let safeUrlString = url {
+                AsyncImage(url: URL(string: safeUrlString))
+            }
+            Button {
+                let pasteboard = UIPasteboard.general
+                
+                if pasteboard.hasStrings {
+                    url = address + pasteboard.string!
+                }
+            } label: {
+                Text("Paste")
+            }
+        }
     }
 }
 
